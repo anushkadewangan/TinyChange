c70975c98c5bcc6b03d6b89ed0c7ef628577d3d7
adewangan16 <adewangan@nvidia.com>
2025-03-29 01:08:36 +0530

    adding Budget tracking

--- Tracking/obj/Debug/net8.0/Tracking.AssemblyInfo.cs
+++ Tracking/obj/Debug/net8.0/Tracking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tracking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091884682eac3a2d97595e7506e166a0da3df518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tracking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tracking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
